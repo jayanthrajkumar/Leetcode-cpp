3460268d6eeb4839bd15ad937d08cae25635c074
jayanthraj@04 <127388119+jayanthrajkumar@users.noreply.github.com>
2025-04-02 20:48:14 +0530

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    int bs(vector<int>&nums,int low,int high,int target){
+        if(low>high)return -1;
+        int mid=(low+high)/2;
+        if(nums[mid]==target)return mid;
+        else if(nums[mid]>target)return bs(nums,low,mid-1,target);
+        return bs(nums,mid+1,high,target);
+    }
+        int search(vector<int>& nums, int target) {
+            int n=nums.size();
+            return bs(nums,0,n-1,target);
+        }
+    };
